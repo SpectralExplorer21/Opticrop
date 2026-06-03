aace51eb67dfeaeaf8a157f34a52596249facfb0
SpectralExplorer21 <kayo09841@gmail.com>
2026-06-03 17:06:51 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title(" OptiCrop")
+st.subheader("AI-powered Soil Intelligence System")
+
+st.write("Welcome to OptiCrop dashboard")
+
+st.write("Use the sidebar to navigate:")
+st.write("️ Analysis")
+st.write("️ History")
+st.write("️ Recommendations")
